3a58f86821ee7698d383480717971836a0833923
Fahad <sfahad_hus@yahoo.com>
2020-07-31 21:21:39 -0400

    fetched news data using componentdidmount

--- src/components/news/searchNews.jsx
+++ src/components/news/searchNews.jsx
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 
-console.log(process.env.REACT_APP_CURRENTS_API_KEY);
+const URL = "https://api.currentsapi.services/v1/latest-news?";
+const API_KEY = process.env.REACT_APP_CURRENTS_API_KEY;
 
 class searchNews extends Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            news: []
+        }
+    }
+
+    componentDidMount() {
+        fetch(`${URL}apiKey=${API_KEY}`)
+            .then(resp => resp.json())
+            .then(json => this.setState({ news: json }))
+            .catch(err => console.log(err));
     }
 
     render() {
+        console.log(this.state.news);
         return (
             <div>
                 <h1>Hello</h1>
