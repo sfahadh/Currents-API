496b180ae3baed2f01b582a7cf05a67023355e87
Fahad <sfahad_hus@yahoo.com>
2020-08-01 20:59:13 -0400

    installed font awesome

--- src/components/home/searchbar.jsx
+++ src/components/home/searchbar.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './home.css'
 
 const searchBar = () => {
     return (
         <>
             <h1 className="text-center display-3 mt-5">Welcome to Current News</h1>
-            <div class="input-group mb-3">
-                <div class="input-group-prepend">
-                    <span class="input-group-text" id="basic-addon1">@</span>
-                </div>
-                <input type="text"
-                    class="form-control"
-                    placeholder="search for news" />
-            </div>
+            <FontAwesomeIcon icon="search" />
+            <form class="form-inline d-flex justify-content-center md-form form-sm mt-0">
+                <input class="form-control form-control-sm ml-3 w-50" type="text" placeholder="Search"
+                    aria-label="Search" />
+            </form>
         </>
     );
 }
