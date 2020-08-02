fe4e063fe677f5977ca1199595b65f4c7d3160a1
Fahad <sfahad_hus@yahoo.com>
2020-08-01 20:04:22 -0400

    styled the header in searchbar component and added input element

--- src/components/home/searchbar.jsx
+++ src/components/home/searchbar.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import './home.css'
 
 const searchBar = () => {
     return (
         <>
-            <h1>Welcome to Current News</h1>
+            <h1 className="text-center display-3 mt-5">Welcome to Current News</h1>
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="basic-addon1">@</span>
+                </div>
+                <input type="text"
+                    class="form-control"
+                    placeholder="search for news" />
+            </div>
         </>
     );
 }
