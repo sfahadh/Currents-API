64fea82b7220799db06e610877bfe6cbf6b215b3
Fahad <sfahad_hus@yahoo.com>
2020-08-03 11:05:36 -0400

    added url, apiKey, and state in categorizedNews file

--- src/components/news/categorizedNews/categorizedNews.jsx
+++ src/components/news/categorizedNews/categorizedNews.jsx
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 
+const URL = "https://api.currentsapi.services/v1/latest-news?category=";
+const API_KEY = process.env.REACT_APP_CURRENTS_API_KEY;
+
 class categorizedNews extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            data: []
+        }
     }
+
 
     render() {
         return (
