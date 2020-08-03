5fbd1f344a70333b0056f94f71e69becada30a6a
Fahad <sfahad_hus@yahoo.com>
2020-08-03 10:57:19 -0400

    added categorizedNews article

---
+++ src/components/news/categorizedNews/categorizedNews.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class categorizedNews extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <>
+            </>
+        )
+    }
+}
+
+export default categorizedNews;
