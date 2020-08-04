dbdcad8d376b9f5c485037ea7bae13670d9d4cab
Fahad <sfahad_hus@yahoo.com>
2020-08-04 19:17:53 -0400

    added constructor to searchNews.jsx file

--- src/components/news/searchNews/searchNews.jsx
+++ src/components/news/searchNews/searchNews.jsx
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
+import '../news.css'
+
+const URL = "https://api.currentsapi.services/v1/search?keywords=";
+const API_KEY = process.env.REACT_APP_CURRENTS_API_KEY;
 
 class searchNews extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: []
+        }
+    }
 
     render() {
         return (
